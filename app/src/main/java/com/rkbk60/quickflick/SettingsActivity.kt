9df38cadd55b36ceb332c87a2f334faa8cf63f46
rkbk60 <b8csmlplg@gmail.com>
2017-10-25 19:38:23 +0900

    add: key height setting

--- app/src/main/java/com/rkbk60/quickflick/SettingsActivity.kt
+++ app/src/main/java/com/rkbk60/quickflick/SettingsActivity.kt
@@ -25,6 +25,7 @@ class SettingsActivity: AppCompatActivity() {
 
         private val TYPE_THRESHOLD = 0
         private val TYPE_THEME = 1
+        private val TYPE_HEIGHT = 2
 
         private val minimalThreshold = 10
 
@@ -33,6 +34,7 @@ class SettingsActivity: AppCompatActivity() {
         private lateinit var thresholdX2: PreferenceTuple
         private lateinit var thresholdY1: PreferenceTuple
         private lateinit var thresholdY2: PreferenceTuple
+        private lateinit var keysHeight: PreferenceTuple
         private lateinit var themeIndicator: PreferenceTuple
 
         private lateinit var localContext: Context
@@ -49,6 +51,7 @@ class SettingsActivity: AppCompatActivity() {
             super.onResume()
             preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
             updateAllThresholdSummary()
+            updateKeysHeightSummary()
             updateThemeSummary()
         }
 
@@ -63,6 +66,7 @@ class SettingsActivity: AppCompatActivity() {
                 thresholdX2.keyName -> thresholdX2
                 thresholdY1.keyName -> thresholdY1
                 thresholdY2.keyName -> thresholdY2
+                keysHeight.keyName -> keysHeight
                 themeIndicator.keyName -> themeIndicator
                 else -> return
             }
@@ -70,6 +74,10 @@ class SettingsActivity: AppCompatActivity() {
                 TYPE_THRESHOLD -> {
                     validateThreshold(sharedPreferences, target)
                     updateThresholdSummary(sharedPreferences, target)
+                }
+                TYPE_HEIGHT -> {
+                    validateKeysHeight(sharedPreferences)
+                    updateKeysHeightSummary(sharedPreferences)
                 }
                 TYPE_THEME -> {
                     updateThemeSummary(sharedPreferences)
@@ -99,6 +107,11 @@ class SettingsActivity: AppCompatActivity() {
                     resources.getInteger(R.integer.preferences_y2_default),
                     TYPE_THRESHOLD
             )
+            keysHeight = PreferenceTuple(
+                    resources.getString(R.string.preferences_keys_height),
+                    resources.getInteger(R.integer.preferences_keys_height_default),
+                    TYPE_HEIGHT
+            )
             themeIndicator = PreferenceTuple(
                     resources.getString(R.string.preferences_theme_indicator),
                     resources.getString(R.string.theme_base),
@@ -123,6 +136,32 @@ class SettingsActivity: AppCompatActivity() {
             }
         }
 
+        private fun validateKeysHeight(sharedPreferences: SharedPreferences?) {
+            val newValue = sharedPreferences?.getString(keysHeight.keyName, "") ?: return
+            var fix = 0
+            val min = 32
+            val max = 48
+            when {
+                (newValue.trim() == "") -> {
+                    toast("Set default value.")
+                    fix = keysHeight.default.toString().toInt()
+                }
+                (newValue.toInt() < min) -> {
+                    toast("Minimal key height value is ${min}dp.")
+                    fix = min
+                }
+                (newValue.toInt() > max) -> {
+                    toast("Maximal key height value is ${max}dp.")
+                    fix = max
+                }
+            }
+            if (fix > 0) {
+                preferenceScreen.sharedPreferences.edit()
+                        .putString(keysHeight.keyName, fix.toString())
+                        .commit()
+            }
+        }
+
         private fun updateThresholdSummary(sharedPreferences: SharedPreferences?, target: PreferenceTuple) {
             if (target.type != TYPE_THRESHOLD) return
             val preference = sharedPreferences ?: preferenceScreen.sharedPreferences
@@ -134,6 +173,13 @@ class SettingsActivity: AppCompatActivity() {
         private fun updateAllThresholdSummary() {
             val targets = listOf(thresholdX1, thresholdX2, thresholdY1, thresholdY2)
             for (target in targets) updateThresholdSummary(preferenceScreen.sharedPreferences, target)
+        }
+
+        private fun updateKeysHeightSummary(sharedPreferences: SharedPreferences? = null) {
+            val preference = sharedPreferences ?: preferenceScreen.sharedPreferences
+            val default = keysHeight.default
+            val newValue = preference.getString(keysHeight.keyName, default.toString())
+            findPreference(keysHeight.keyName).summary = "${newValue}dp (Default:$default)"
         }
 
         private fun updateThemeSummary(sharedPreferences: SharedPreferences? = null) {
