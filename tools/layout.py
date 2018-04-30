d40c6c76f34e6e607e66157b11736f27391938f1
rkbk60 <b8csmlplg@gmail.com>
2018-04-30 19:27:25 +0900

    correspond to key width on keyboard.xml generator

--- tools/layout.py
+++ tools/layout.py
@@ -1,4 +1,5 @@
 
+import itertools
 import io
 import os
 import shutil
@@ -6,36 +7,58 @@ import shutil
 DEBUG_MODE = False
 
 
-class Dimention:
-    def __init__(self, name: str) -> None:
+class Height:
+    def __init__(self, name: str, height_level: int) -> None:
         self.name = name
+        self.id = str(height_level)
         self.value = "key_height_" + name
+        self.info = "@dimen/%s (Lv.%d)" % (self.value, height_level)
 
 
 class Orientation:
     def __init__(self, name: str, value: int) -> None:
         self.name = name
+        self.id = name[0:2].capitalize()
         self.value = "%ddp" % value
+        self.info = "for %s (footer:%s)" % (name, self.value)
 
 
-dimentions = [
-    Dimention("small"),
-    Dimention("semi_small"),
-    Dimention("medium"),
-    Dimention("semi_large"),
-    Dimention("large")
+class Adjustment:
+    def __init__(self, is_right: bool) -> None:
+        self.name = "right" if is_right else "left"
+        self.id = self.name[0:1].upper()
+        if is_right:
+            self.width_left = "17%p"
+            self.width_right = "0"
+        else:
+            self.width_left = "0"
+            self.width_right = "17%p"
+        self.info = "for " + self.name + " hand"
+
+
+heights = [
+    Height("small", 1),
+    Height("semi_small", 2),
+    Height("medium", 3),
+    Height("semi_large", 4),
+    Height("large", 5)
 ]
 
-pr = Orientation("portrait", 8)
-ls = Orientation("landscape", 16)
-portraits = list(map(lambda x: (x, pr), dimentions))
-landscapes = list(map(lambda x: (x, ls), dimentions))
+orientations = [
+    Orientation("portrait", 8),
+    Orientation("landscape", 16)
+]
+
+adjustments = [
+    Adjustment(False),
+    Adjustment(True)
+]
 
 
 if DEBUG_MODE:
-    orders = [(Dimention("tD"), Orientation("tO", 10))]
+    orders = [(Height("tD", 0), Orientation("tO", 10), Adjustment(True))]
 else:
-    orders = portraits + landscapes
+    orders = list(itertools.product(heights, orientations, adjustments))
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 output_dir = current_dir + "/output/keyboard/"
@@ -43,15 +66,21 @@ if os.path.exists(output_dir):
     shutil.rmtree(output_dir)
 os.makedirs(output_dir)
 
-for (dim, ori) in orders:
+for (hei, ori, adj) in orders:
     template = io.open("%s/template/keyboard.xml" % current_dir)
     newtext = ""
     for newline in template:
-        newline = newline.replace("$KEY_HEIGHT_VALUE", dim.value)
+        newline = newline.replace("$KEY_HEIGHT_INFO", hei.info)
+        newline = newline.replace("$KEY_HEIGHT_VALUE", hei.value)
+        newline = newline.replace("$ORIENTATION_INFO", ori.info)
         newline = newline.replace("$FOOTER_HEIGHT_DP", ori.value)
+        newline = newline.replace("$ADJUSTMENT_INFO", adj.info)
+        newline = newline.replace("$KEY_WIDTH_LEFT", adj.width_left)
+        newline = newline.replace("$KEY_WIDTH_RIGHT", adj.width_right)
         newtext += newline
     template.close()
-    filename = "keyboard_%s_%s.xml" % (dim.name, ori.name)
+    filename = "keyboard_%s_%s_%s.xml" % (hei.name, ori.name, adj.name)
+    filename = "keyboard_" + adj.id + ori.id + hei.id + ".xml"
     newxml = io.open(output_dir + filename, "w+")
     newxml.write(newtext)
     newxml.close()
