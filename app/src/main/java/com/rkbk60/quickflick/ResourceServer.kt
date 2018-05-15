1d067d274f3ddb0e2350bf96710e5ea93a39257b
rkbk60 <b8csmlplg@gmail.com>
2018-05-15 12:07:14 +0900

    refactor: make ResourceServerBase

--- app/src/main/java/com/rkbk60/quickflick/ResourceServer.kt
+++ app/src/main/java/com/rkbk60/quickflick/ResourceServer.kt
@@ -1,95 +1,13 @@
 package com.rkbk60.quickflick
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.graphics.PorterDuff
 import android.graphics.drawable.BitmapDrawable
-import android.preference.PreferenceManager
 import android.support.annotation.*
-import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
 import com.rkbk60.quickflick.domain.IndicatorFactory
 
-class ResourceServer(private val context: Context) {
-    private val preference = PreferenceManager.getDefaultSharedPreferences(context)
-    private val editor = preference.edit()
-
-    /**
-     * Preference object that value corresponds kotlin's primitive type.
-     * @param keyId preference key name id
-     * @param default default value
-     */
-    abstract inner class PreferenceData <T: Any> (@StringRes keyId: Int, private val default: T) {
-        protected val key: String = context.getString(keyId) ?: throw Error("failed to detect key name id")
-        protected abstract val getT: ()  -> T?
-        protected abstract val setT: (T) -> SharedPreferences.Editor
-        /**
-         * Current preference value.
-         */
-        var current: T
-            get() = getT() ?: default
-            set(value) { setT(value).commit() }
-
-        override fun toString(): String = "$key: $current(default: $default)"
-    }
-
-    /**
-     * Preference object that value is Int but internal type is String.
-     * @param keyId preference key name id
-     * @param default default value
-     */
-    inner class PreferenceIntText(@StringRes keyId: Int, default: Int) : PreferenceData<Int>(keyId, default) {
-        override val getT = { preference.getString(key, default.toString()).toInt() }
-        override val setT = { value: Int -> editor.putString(key, value.toString()) }
-    }
-
-    /**
-     * Preference object that value corresponds kotlin's Boolean type.
-     * @param keyId preference key name id
-     * @param default default value
-     */
-    inner class PreferenceBool(@StringRes keyId: Int, default: Boolean) : PreferenceData<Boolean>(keyId, default) {
-        override val getT = { preference.getBoolean(key, default) }
-        override val setT = { value: Boolean -> editor.putBoolean(key, value) }
-    }
-
-    interface ResourceEnum {
-        @StringRes
-        fun toResourceId(): Int
-    }
-
-    /**
-     * Preference object that value corresponds kotlin's enum class.
-     * @param keyId preference key name id
-     * @param default default value
-     * @param candidates enumValues() or values that this application may use
-     */
-    inner class PreferenceEnum <T>(@StringRes keyId: Int,
-                                   private val default: T,
-                                   private val candidates: Array<T>)
-            where T : Enum<T>, T : ResourceEnum {
-        private val key = context.getString(keyId) ?: throw Error("failed to detect key string id")
-        private val defaultString = resStringOf(default)
-        private val candidateMap = candidates.map { Pair(it, resStringOf(it)) }
-
-        /**
-         * Current preference value.
-         */
-        var current: T
-            get() {
-                val current = preference.getString(key, defaultString)
-                return candidateMap.firstOrNull { it.second == current }?.first ?: default
-            }
-            set(value) {
-                if (!candidates.contains(value)) return
-                editor.putString(key, resStringOf(value)).commit()
-            }
-
-        override fun toString(): String = "$key: $current(default: $default)"
-
-        private fun resStringOf(value: T): String = context.getString(value.toResourceId())
-    }
-
+class ResourceServer(context: Context) : ResourceServerBase(context) {
     val thresholdX1 = PreferenceIntText(R.string.preferences_x1, 25)
     val thresholdX2 = PreferenceIntText(R.string.preferences_x2, 150)
     val thresholdY1 = PreferenceIntText(R.string.preferences_y1, 25)
@@ -100,7 +18,6 @@ class ResourceServer(private val context: Context) {
     val keyboardUseFooter = PreferenceBool(R.string.preferences_keyboard_footer, false)
 
     val keyboardHeight = PreferenceEnum(R.string.preferences_keyboard_height, KeyboardHeight.Lv2, enumValues())
-
     enum class KeyboardHeight : ResourceEnum {
         Lv1, Lv2, Lv3, Lv4, Lv5;
 
@@ -124,7 +41,6 @@ class ResourceServer(private val context: Context) {
     private val indicatorTheme = PreferenceEnum(R.string.preferences_theme_indicator,
                                                 IndicatorTheme.Base,
                                                 enumValues())
-
     private enum class IndicatorTheme : ResourceEnum {
         Base, Lime, Morse;
 
@@ -181,16 +97,4 @@ class ResourceServer(private val context: Context) {
         }
         return backgrounds
     }
-
-    // sugar-syntax methods to access resources
-
-    private fun pixelResourceOf(@DimenRes id: Int) =
-            context.resources.getDimensionPixelSize(id)
-
-    @ColorInt
-    private fun colorResourceOf(@ColorRes id: Int) =
-            ContextCompat.getColor(context, id)
-
-    private fun bitmapResourceOf(@DrawableRes id: Int): BitmapDrawable =
-            ContextCompat.getDrawable(context, id)?.mutate() as BitmapDrawable
 }
---
+++ app/src/main/java/com/rkbk60/quickflick/ResourceServerBase.kt
@@ -0,0 +1,109 @@
+package com.rkbk60.quickflick
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.drawable.BitmapDrawable
+import android.preference.PreferenceManager
+import android.support.annotation.*
+import android.support.v4.content.ContextCompat
+
+/**
+ * Class that defines functions/classes using in ResourceServer.
+ * @param context context
+ */
+abstract class ResourceServerBase(protected val context: Context) {
+    private val preference = PreferenceManager.getDefaultSharedPreferences(context)
+    private val editor = preference.edit()
+
+    /**
+     * Preference object that value corresponds kotlin's primitive type.
+     * @param keyId preference key name id
+     * @param default default value
+     */
+    abstract inner class PreferenceData <T: Any> (@StringRes keyId: Int, private val default: T) {
+        protected val key: String = context.getString(keyId) ?: throw Error("failed to detect key name id")
+        protected abstract val getT: ()  -> T?
+        protected abstract val setT: (T) -> SharedPreferences.Editor
+        /**
+         * Current preference value.
+         */
+        var current: T
+            get() = getT() ?: default
+            set(value) { setT(value).commit() }
+
+        override fun toString(): String = "$key: $current(default: $default)"
+    }
+
+    /**
+     * Preference object that value is Int but internal type is String.
+     * @param keyId preference key name id
+     * @param default default value
+     */
+    inner class PreferenceIntText(@StringRes keyId: Int, default: Int) : PreferenceData<Int>(keyId, default) {
+        override val getT = { preference.getString(key, default.toString()).toInt() }
+        override val setT = { value: Int -> editor.putString(key, value.toString()) }
+    }
+
+    /**
+     * Preference object that value corresponds kotlin's Boolean type.
+     * @param keyId preference key name id
+     * @param default default value
+     */
+    inner class PreferenceBool(@StringRes keyId: Int, default: Boolean) : PreferenceData<Boolean>(keyId, default) {
+        override val getT = { preference.getBoolean(key, default) }
+        override val setT = { value: Boolean -> editor.putBoolean(key, value) }
+    }
+
+    /**
+     * Interface or convert enum value to resource string.
+     * Classes that use in [PreferenceEnum] have to implement this.
+     */
+    interface ResourceEnum {
+        @StringRes
+        fun toResourceId(): Int
+    }
+
+    /**
+     * Preference object that value corresponds kotlin's enum class.
+     * @param keyId preference key name id
+     * @param default default value
+     * @param candidates enumValues() or values that this application may use
+     */
+    inner class PreferenceEnum <T>(@StringRes keyId: Int,
+                                   private val default: T,
+                                   private val candidates: Array<T>)
+            where T : Enum<T>, T : ResourceEnum {
+        private val key = context.getString(keyId) ?: throw Error("failed to detect key string id")
+        private val defaultString = resStringOf(default)
+        private val candidateMap = candidates.map { Pair(it, resStringOf(it)) }
+
+        /**
+         * Current preference value.
+         */
+        var current: T
+            get() {
+                val current = preference.getString(key, defaultString)
+                return candidateMap.firstOrNull { it.second == current }?.first ?: default
+            }
+            set(value) {
+                if (!candidates.contains(value)) return
+                editor.putString(key, resStringOf(value)).commit()
+            }
+
+        override fun toString(): String = "$key: $current(default: $default)"
+
+        private fun resStringOf(value: T): String = context.getString(value.toResourceId())
+    }
+
+    // sugar-syntax methods to access resources
+
+    protected fun pixelResourceOf(@DimenRes id: Int) =
+            context.resources.getDimensionPixelSize(id)
+
+    @ColorInt
+    protected fun colorResourceOf(@ColorRes id: Int) =
+            ContextCompat.getColor(context, id)
+
+    protected fun bitmapResourceOf(@DrawableRes id: Int): BitmapDrawable =
+            ContextCompat.getDrawable(context, id)?.mutate() as BitmapDrawable
+}
